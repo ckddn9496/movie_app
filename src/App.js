9dbf4bbba30c9c01540e921785fa5902ef207827
ckddn9496 <ckddn9496@gmail.com>
2021-02-20 00:33:47 +0900

    add HashRouter, Navigation, Route, Link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { HashRouter, Route } from "react-router-dom";
-import About from "./routes/About";
-import Home from "./routes/Home";
-
+import { HashRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import About from './routes/About';
+import Home from './routes/Home';
 
 function App() {
-    return <HashRouter>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={About} />
-    </HashRouter>
+    return (
+        <HashRouter>
+            <Navigation />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+        </HashRouter>
+    );
 }
 
 export default App;
